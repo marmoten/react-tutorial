bacc9d41281644bd3c95a859d95dde3a87c1afa2
David Wiggins <dwiggins@expedia.com>
2019-07-28 11:47:56 -0700

    Updates

---
+++ precache-manifest.b401fe0a4b84eca44537c5f2c31c3d67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9a413b7a37f722302a07c8ad4b73a4",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2f45284bc4327117a81f",
+    "url": "/react-tutorial/static/css/main.1a942d58.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "2f45284bc4327117a81f",
+    "url": "/react-tutorial/static/js/main.f9ca51de.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
